1f6734917141152ea0caa04c84cdd1616cb51540
Seira Yun <discoverdeepseira@gmail.com>
2021-08-11 17:00:33 +0900

    airbnb incomplete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning the db..."
+Flat.destroy_all
+
+puts "Creating flats..."
+50.times do
+  Flat.create!(
+    name: Faker::Mountain.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraphs(number: 1),
+    price_per_night: rand(30..100),
+    number_of_guests: rand(1..6)
+  )
+end
+puts "Created #{Flat.count} flats."
